61cc4c83d8284b9aa590241aeb86eb571d9ad894
Przemo <przemo06133@gmail.com>
2023-05-16 20:04:15 +0200

    Init contract with context

---
+++ client/src/context/index.jsx
@@ -0,0 +1,76 @@
+import React, { createContext, useContext, useEffect, useRef, useState } from 'react';
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal';
+import { useNavigate } from 'react-router-dom';
+
+// import { GetParams } from '../utils/onboard.js';
+import { ABI, ADDRESS } from '../contract';
+// import { createEventListeners } from './createEventListeners';
+
+const GlobalContext = createContext()
+
+export const GlobalContextProvider = ({ children }) => {
+    const [walletAddress, setWalletAddress] = useState('');
+    const [battleGround, setBattleGround] = useState('bg-astral');
+    const [contract, setContract] = useState(null);
+    const [provider, setProvider] = useState(null);
+    const [step, setStep] = useState(1);
+    const [gameData, setGameData] = useState({ players: [], pendingBattles: [], activeBattle: null });
+    const [showAlert, setShowAlert] = useState({ status: false, type: 'info', message: '' });
+    const [battleName, setBattleName] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [updateGameData, setUpdateGameData] = useState(0);
+
+    //* Set the wallet address to the state
+    const updateCurrentWalletAddress = async () => {
+        const accounts = await window?.ethereum?.request({ method: 'eth_requestAccounts' });
+        console.log('accs', accounts)
+        if (accounts) setWalletAddress(accounts[0]);
+    };
+
+    useEffect(() => {
+        if (walletAddress) return
+        updateCurrentWalletAddress()
+        window?.ethereum?.on('accountsChanged', updateCurrentWalletAddress);
+    }, [])
+
+    //* Set the smart contract and provider to the state
+    useEffect(() => {
+        const setSmartContractAndProvider = async () => {
+            const web3Modal = new Web3Modal();
+            const connection = await web3Modal.connect();
+            const newProvider = new ethers.providers.Web3Provider(connection);
+            const signer = newProvider.getSigner();
+            const newContract = new ethers.Contract(ADDRESS, ABI, signer);
+
+            setProvider(newProvider);
+            setContract(newContract);
+        };
+        setSmartContractAndProvider();
+    }, []);
+
+    return (
+        <GlobalContext.Provider
+            value={{
+                // player1Ref,
+                // player2Ref,
+                // battleGround,
+                // setBattleGround,
+                contract,
+                // gameData,
+                walletAddress,
+                // updateCurrentWalletAddress,
+                // showAlert,
+                // setShowAlert,
+                // battleName,
+                // setBattleName,
+                // errorMessage,
+                // setErrorMessage,
+            }}
+        >
+            {children}
+        </GlobalContext.Provider>
+    );
+};
+
+export const useGlobalContext = () => useContext(GlobalContext);
---
+++ client/src/contract/index.js
@@ -0,0 +1,4 @@
+import contract from './AVAXGods.json'
+
+export const ADDRESS = '0x566FeB5af6626E5A6393f690891C951493E5C1dF'
+export const {abi: ABI} = contract
